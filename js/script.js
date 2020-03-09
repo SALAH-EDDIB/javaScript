0c1d8706eb98e888f34d633463ff4e88375731a2
salah eddib <salaheddib.55@gmail.com>
2020-03-09 20:28:36 +0100

    end writing javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 var favoriteBooks = ['the witcher', 'To Kill a Mockingbird', 'Invisible Man'];
+
+function addFavoriteBook(bookname) {
+    var n = bookname.includes("Great")
+    if (n != true) {
+        favoriteBooks.push(bookname);
+    }
+}
+
+function printFavoriteBooks() {
+
+    console.log("Livres favoris : " + favoriteBooks[0])
+    console.log("number of books :  " + favoriteBooks.length)
+
+    for (let y = 0; y < favoriteBooks.length; y++) {
+
+        console.log(favoriteBooks[y]);
+
+    }
+}
