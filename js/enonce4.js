60da94c7d4d8334ac65f6832bbe8e81d9a576e2e
salah eddib <salaheddib.55@gmail.com>
2020-03-22 08:42:01 +0100

    end of enonce 6

--- js/enonce4.js
+++ js/enonce4.js
@@ -55,3 +55,71 @@ function updateRecords(id, prop, value) {
 
     return collection[id];
 }
+
+
+// var contacts = [{
+//     "firstName": "Akira",
+//     "lastName": "Laine",
+//     "number": "0543236543",
+//     "likes": ["Pizza", "Coding", "Brownie Points"]
+// },
+// {
+//     "firstName": "Harry",
+//     "lastName": "Potter",
+//     "number": "0994372684",
+//     "likes": ["Hogwarts", "Magic", "Hagrid"]
+// },
+// {
+//     "firstName": "Sherlock",
+//     "lastName": "Holmes",
+//     "number": "0487345643",
+//     "likes": ["Intriguing Cases", "Violin"]
+// },
+// {
+//     "firstName": "Kristian",
+//     "lastName": "Vos",
+//     "number": "unknown",
+//     "likes": ["JavaScript", "Gaming", "Foxes"]
+// },
+// {
+//     "Akira": 0,
+//     "Harry": 1,
+//     "Sherlock": 2,
+//     "Kristian": 3,
+
+
+// }
+
+// ];
+
+// let exist = false;
+
+// function lookUpProfile(name, prop) {
+
+// for (i = 0; i < contacts.length - 1; i++) {
+
+//     if (contacts[i].firstName.includes(name)) {
+
+//         exist = true;
+//         if (contacts[i][prop] !== undefined) {
+
+//             console.log(contacts[contacts[4][name]][prop])
+//         } else {
+//             console.log('Aucune propriété de ce type')
+//         }
+
+//     } else if (!contacts[i].firstName.includes(name) && exist === false) {
+//         console.log('Aucun contact')
+//     }
+
+
+// }
+
+
+
+
+
+
+// }
+
+// lookUpProfile("Akia", "likes");
